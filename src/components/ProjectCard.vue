d4aa45b913e56696f93e4aacebc95a6744b477da
aletardio <ale.tardio99@gmail.com>
2024-03-03 14:08:52 +0100

    Added image in each card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -13,7 +13,16 @@ export default {
     created() {
     },
     methods: {
-
+        getUrlImg(){
+            let image;
+            if (this.project.cover_image != null) {
+                image = 'storage/' + this.project.cover_image;
+            }
+            else {
+                image = 'img/placeholder.webp';
+            }
+            return `${this.store.baseUrl}/${image}`;
+        }
     }
 }
 </script>
@@ -21,6 +30,7 @@ export default {
 
 <div class="col-4">
     <div class="card my-3 shadow">
+        <img class="rounded-top-1":src="getUrlImg()">
         <div class="card-body ">
             <h5 class="card-title text-center my-2">{{project.title}}</h5>
             <p class="card-text "><strong>Descrizione:</strong> {{project.description.substr(0,80)+"..."}}</p>
