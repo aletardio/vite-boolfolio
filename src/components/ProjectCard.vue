dd7b30b91a89ca397628367d5288e87bbf0d2c2c
aletardio <ale.tardio99@gmail.com>
2024-03-04 11:51:17 +0100

    Assigned color in each tecnology name

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -22,7 +22,18 @@ export default {
                 image = 'img/placeholder.webp';
             }
             return `${this.store.baseUrl}/${image}`;
-        }
+        },
+        getTechnologyColor(slug) {
+        const colorMap = {
+            html: '#F46B22',
+            css: '#2D53E5',
+            javascript: '#F7E029',
+            vuejs: '#41D392',
+            php: '#7986B8',
+            laravel: '#F33A2E'
+        };
+        return colorMap[slug] || '#000000'; 
+        },
     }
 }
 </script>
@@ -31,15 +42,23 @@ export default {
 <div class="col-4">
     <div class="card my-3 shadow">
         <img class="rounded-top-1":src="getUrlImg()">
-        <div class="card-body ">
+        <div class="card-body">
             <h5 class="card-title text-center my-2">{{project.title}}</h5>
             <p class="card-text "><strong>Descrizione:</strong> {{project.description.substr(0,70)+"..."}}</p>
             <p class="card-text "><strong>Link:</strong> {{project.link.substr(0,30)+"..."}}</p>
+            <p class="card-text"><strong>Tecnologia: </strong> 
+                <button v-for="(technology, index) in project.technologies" :key="index" class="btn btn-sm m-1 text-white" 
+                :style="{ backgroundColor: getTechnologyColor(technology.slug) }">
+                    {{ technology.name }}
+                </button>
+            </p>
         </div>
     </div>
 </div>
 
 </template>
 <style lang="scss" scoped>
-    
+    // .card.my-3.shadow {
+    // height: 450px;
+    // }
 </style>
