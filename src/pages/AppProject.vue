c61ce7c44f549c52cb33c7f83d8c55d9abc69062
aletardio <ale.tardio99@gmail.com>
2024-03-05 15:37:46 +0100

    Show types

--- src/pages/AppProject.vue
+++ src/pages/AppProject.vue
@@ -12,13 +12,17 @@ export default {
     data() {
         return {
             projects:[],
+            types:[],
             store,
-            currentPage: 1,
-            lastPage: null
+            currentPageProjects: 1,
+            lastPageProjects: null,
+            currentPageTypes: 1,
+            lastPageTypes: null,
         }
     },
   created() {
     this.getProjects();
+    this.getTypes();
   },
   methods: {
     getProjects(page_num){
@@ -28,8 +32,19 @@ export default {
         }
       }).then((response) => {
         this.projects = response.data.results.data;
-        this.currentPage = response.data.results.current_page;
-        this.lastPage = response.data.results.last_page;
+        this.currentPageProjects = response.data.results.current_page;
+        this.lastPageProjects = response.data.results.last_page;
+      })
+    },
+    getTypes(page_num){
+      axios.get(`${this.store.baseUrl}/api/types`, {
+        params: {
+            page: page_num
+        }
+      }).then((response) => {
+        this.types = response.data.results.data;
+        this.currentPageTypes = response.data.results.current_page;
+        this.lastPageTypes = response.data.results.last_page;
       })
     }
   },
@@ -46,6 +61,13 @@ export default {
                     <p class="text-center">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam optio totam esse voluptate placeat, dicta iure debitis quaerat explicabo et quis quisquam dolores, unde ipsa consectetur accusantium vel laudantium in!
                     </p>
+                </div>
+                <div class="row">
+                    <div>
+                    <span class="badge rounded-pill me-2 bg-color-black" v-for="type, index in types" :key="index" >
+                        {{ type.name }}
+                    </span>
+                    </div>
                 </div>
                 <div class="row">
                     <ProjectCard v-for="project, index in projects" :key="index" :project="project"/>
@@ -75,5 +97,14 @@ export default {
     .btn-page {
         background-color: #252525;
         color: #fff;
+    }
+
+    span.badge.rounded-pill.me-2.bg-color-black {
+        background-color:#000;
+        color: #fff;
+        cursor: pointer;
+        &:hover{
+            background-color: rgb(149, 149, 149);
+        }
     }
 </style>./AppMain.vue/index.js
